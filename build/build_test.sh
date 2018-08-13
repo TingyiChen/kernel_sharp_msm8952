18c85c2c40de94010484b3923485d29bf07ff336
Isaac Chen <isaacchen@isaacchen.cn>
2018-08-13 12:45:00 -0400

    S0007
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- build/build_test.sh
+++ build/build_test.sh
@@ -11,11 +11,17 @@ export BUILD_CONFIG=build/build.config.net_test
 test=all_tests.sh
 set -e
 source ${ROOT_DIR}/envsetup.sh
+export OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
+mkdir -p ${OUT_DIR}
+
+# Normally this comes from build.config but build.config.net_test
+# is generic, assume there is only one source directory.
+export KERNEL_DIR=$(readlink -m ${KERNEL_DIR}/*)
 
 echo "========================================================"
 echo " Building kernel and running tests "
 
-cd ${KERNEL_DIR}/*
+cd ${OUT_DIR}
 $NET_TEST/run_net_test.sh --builder $test
 
 echo "======Finished running tests======"
